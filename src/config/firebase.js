8d2e34dc683d00089653d92f508bbd3a4754f4ad
xspize <4PEREJ88@solent.ac.uk>
2023-01-20 05:16:55 +0000

    added project to firebase hosting, & other minor changes e.g. title

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+
+	apiKey: "AIzaSyBPGHlHv49O0RnMHxs7Hiyl4MK-R0jXc64",
+	authDomain: "learned-trilogy-192223.firebaseapp.com",
+	projectId: "learned-trilogy-192223",
+	storageBucket: "learned-trilogy-192223.appspot.com",
+	messagingSenderId: "584449908017",
+	appId: "1:584449908017:web:a9f0c6b62fea8d5320a410",
+	measurementId: "G-FZSMH7MYEJ"
+  };
+
+export default firebaseConfig; 
